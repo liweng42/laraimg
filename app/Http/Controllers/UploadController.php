c52498082b2b19cd64e74de08af18f56915e3735
liweng42 <liweng42@gmail.com>
2019-11-29 17:18:38 +0800

    更新上传方法

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\File;
+
+
 
 class UploadController extends Controller
 {
     //
     private $resource_path = 'upload';
+    private $accessId = '1';
+    private $accessKey = 'abc';
 
     public function index(){
         return view('upload.index',
@@ -16,10 +22,35 @@ class UploadController extends Controller
         ]);
     }
 
-    public  function uploadFile(Request $request){
-        $imageName = $request->file->getClientOriginalName();
-        $request->file->move(public_path('upload'), $imageName);
-    	return response()->json(['uploaded' => '/upload/'.$imageName]);
+    public function uploadFile(Request $request){
+        $accessId = $request['accessId'];
+        $accessKey = $request['accessKey'];
+        $destPath = $request['destPath'];
+        
+        if (!$this->checkAccessIdAndKey($accessId, $accessKey)){
+            return response()->json(['code' => '401','msg' => '$accessKey error.']);
+        }
+        else {
+            //校验目录以及不存在创建
+            $path = public_path($destPath);
+            File::isDirectory($path) or mkdir(iconv("UTF-8", "GBK", $path), 0777, true); 
+            
+            $imageName = $request->file->getClientOriginalName();     
+            $request->file->move($path, $imageName);
+            $imgUrl = URL::to($destPath, $imageName,true);
+            return response()->json(['code' => '200','msg' => 'success', 'uploaded' => $imgUrl ]);
+        }
     }
+
+    public function checkAccessIdAndKey($accessId, $accessKey)
+    {
+        if ($accessId == $this->accessId && $accessKey == $this->accessKey) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
  
 }

90620693d47161903720346ca7d33f8657f40761
liweng42 <liweng42@gmail.com>
2019-12-02 15:09:35 +0800

    修改上传文件根目录

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,11 +14,13 @@ class FileUploadController extends Controller
     private $accessId;
     private $accessKey;
     private $allowedfileExtension;
+    private $uploadRootPath;
 
     public function __construct(){
         $this->accessId = config('accessauth.accessId');
         $this->accessKey = config('accessauth.accessKey');
         $this->allowedfileExtension = config('accessauth.allowedfileExtension');
+        $this->uploadRootPath = storage_path('app/public');
     }
 
     public function index(){
@@ -38,6 +40,7 @@ class FileUploadController extends Controller
             $accessId = $request->accessId;
             // dd($request->accessId);
             $accessKey = $request->accessKey;
+            //destPath 需要带 / 前缀，指向上传文件的根目录
             $destPath = $request->destPath;
             $imgUrls = array();
             if (!$this->checkAccessIdAndKey($accessId, $accessKey)){
@@ -53,8 +56,10 @@ class FileUploadController extends Controller
                         //dd($check);
                         if($check){
                             foreach ($request->photos as $photo) {
-                                //校验目录以及不存在创建
-                                $path = public_path($destPath);
+                                //校验目录以及不存在创建，
+                                //上传文件的根目录是在 /storage/app/public，然后 执行 php artistan storage:link 会在/public目录下创建一个软连接 storage，指向根目录
+                                $path = $this->uploadRootPath.$destPath;
+                                // dd($path);
                                 File::isDirectory($path) or mkdir(iconv("UTF-8", "GBK", $path), 0777, true); 
                                 $imageName = $photo->getClientOriginalName();     
                                 $photo->move($path, $imageName);
--- config/lfm.php
+++ config/lfm.php
@@ -43,10 +43,10 @@ return [
 
     // Which folder to store files in project, fill in 'public', 'resources', 'storage' and so on.
     // You should create routes to serve images if it is not set to public.
-    'base_directory' => 'public',
+    'base_directory' => 'storage/app/public',
 
     'images_folder_name' => 'photos',
-    'files_folder_name'  => 'upload',
+    'files_folder_name'  => '',
 
     'shared_folder_name' => '',
     'thumb_folder_name'  => 'thumbs',

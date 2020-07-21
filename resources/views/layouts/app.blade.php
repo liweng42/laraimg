9a66220ea0979650760a2999cf30bc839f9f247e
liweng42 <liweng42@gmail.com>
2020-07-21 14:26:34 +0800

    修改文件管理器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                             @endif --}}
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="/laravel-filemanager">laravel-filemanager</a>
+                                <a class="nav-link" href="/elfinder">elfinder</a>
                             </li>                        
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>

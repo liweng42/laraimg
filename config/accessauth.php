ec6784c3e9c1af8d55102749e7bc655f6ed7d343
liweng42 <liweng42@gmail.com>
2020-08-18 09:53:42 +0800

    配置到env

--- config/accessauth.php
+++ config/accessauth.php
@@ -10,9 +10,9 @@ return [
     | accessKey
     |
     */    
- 'accessId' => "yKr6a8XifzM9eYPs",
- 'accessKey' => "JpQxByjX1xFMuM96A4rgupvHxBx6QkVF",
- 'allowedfileExtension' => ['jpg','png','jpeg','gif','xlsx'],
+ 'accessId' => env('ACCESSID', "yKr6a8XifzM9eYPs"),
+ 'accessKey' => env('ACCESSKEY', "JpQxByjX1xFMuM96A4rgupvHxBx6QkVF"),
+ 'allowedfileExtension' => ['jpg','png','jpeg','gif'],
  //上传文件的根目录，默认为空，指向网站当前的根目录，即 /public
  'uploadRootPath' => ''
 ];
